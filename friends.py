6e9c377d8975d95c5463c686f63c71941044afe1
GKPriya <43486657+GKPriya@users.noreply.github.com>
2019-02-10 22:03:28 -0800

    Update friends.py

--- friends.py
+++ friends.py
@@ -33,12 +33,12 @@ num_friends=[100, 49, 41, 40, 25, 10, 5, 4, 7, 20, 60]
 # In[2]:
 
 
-print("mean ={} ".format(median_num_friends(num_friends)))
+print("mean ={} ".format(mean_num_friends(num_friends)))
 
 
 
 # In[3]:
 
 
-print("mean ={} ".format(mean_num_friends(num_friends)))
+print("median ={} ".format(median_num_friends(num_friends)))
 
